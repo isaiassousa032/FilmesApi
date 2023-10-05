b1a77f4156fd1fb54a8afaee15c9a3e61056b5f7
Isaias Santos <isaiassousa032@gmail.com>
2023-10-05 15:14:16 -0300

    feat : aplica o automapper e atualiza dados com PUT

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,4 +1,7 @@
-﻿using FilmesApi.Models;
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
@@ -8,27 +11,50 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
 
+    private FilmeContext _context;
+    private IMapper _mapper;
+    
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
 
     [HttpPost]
-    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme(
+        [FromBody] CreateFilmeDto filmeDto)
     {
-        filme.Id = id++;
-        filmes.Add(filme);
-        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+        Filme filme = _mapper.Map<Filme>(filmeDto);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperaFilmePorId),
+            new { id = filme.Id },
+            filme);
     }
 
     [HttpGet]
     public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0,
         [FromQuery] int take = 50)
     {
-        return filmes.Skip(skip).Take(take);
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public IActionResult RecuperaFilmePorId(int id)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = _context.Filmes
+            .FirstOrDefault(filme => filme.Id == id);
         if (filme == null) return NotFound();
         return Ok(filme);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+        return NoContent();
     }
 }
--- FilmesApi/Data/FilmeContext.cs
+++ FilmesApi/Data/FilmeContext.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using FilmesApi.Models;
 
-namespace FilmesApi.Data
-{
-    public class FilmeContext : DbContext
-    {
-        public FilmeContext(DbContextOptions<FilmeContext> options) : base(options)
-        {
-        }
+namespace FilmesApi.Data;
 
-        public DbSet<Filme> Filmes { get; set; }
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> options) : base(options)
+    {
     }
+
+    public DbSet<Filme> Filmes { get; set; }
 }
